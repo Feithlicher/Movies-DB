2e4fc4e7fc0f4c0b6741d6c88fd4ddcd80e166e6
Natan Feithlicher <feithlicher@gmail.com>
2021-05-31 23:26:28 +0300

    added an input field

--- fullStack_project_devclub/react-app/src/App.js
+++ fullStack_project_devclub/react-app/src/App.js
@@ -3,8 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 // import Movies from "./Components/Movies";
 import MoviesHeading from "./Components/MoviesHeading";
-// import SearchBox from "./Components/SearchBox";
-// import SearchBox from "./Components/SearchBox";
+import SearchBox from "./Components/SearchBox";
 // import AddToFavorites from "./Components/AddToFavorites";
 
 const API_KEY = "598d1c39"; // to OMDB
@@ -14,7 +13,7 @@ const App = () => {
     return (
         <div className='first'>
             <MoviesHeading />
-            {/* <SearchBox /> */}
+            <SearchBox />
         </div>
     )
 };
--- fullStack_project_devclub/react-app/src/Components/SearchBox.js
+++ fullStack_project_devclub/react-app/src/Components/SearchBox.js
@@ -1,6 +1,12 @@
 import React from "react";
+import "./SearchBox.css";
 const SearchBox = (props) => {
-    return ();
+    return (
+        <div className="inputDiv">
+            <span className="enterSpan">Enter a movie name:</span>
+            <input className="enterSpan"></input>
+        </div>
+    );
 };
 
 export default SearchBox;
